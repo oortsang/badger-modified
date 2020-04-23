7763f1dd24a2bd29fc21acc1c26b528e570a033e
meliao <owenjmelia@gmail.com>
2020-04-23 09:01:34 -0400

    Added Alvaro's check logs script

---
+++ src/check_logs.py
@@ -0,0 +1,82 @@
+__author__ = "alvaro barbeira"
+import os
+import re
+import pandas
+import logging
+
+from helpers import helpers
+########################################################################################################################
+
+def run(args):
+    subfield_regexp, subfield_names, subfield_positions = helpers.name_parse_prepare(args.name_subfield_regexp, args.name_subfield)
+
+    job_regexp = re.compile(args.jobs_pattern)
+    job_names, job_paths = helpers.parse_with_regexp(args.jobs_folder, job_regexp)
+
+    err_regexp = re.compile(args.logs_pattern)
+    logs_by_name = helpers.identify_with_regexp(args.logs_folder, err_regexp)
+
+    result=[]
+
+    for (i,job_name) in enumerate(job_names):
+        values = helpers.name_parse(job_name, subfield_regexp, subfield_positions) if subfield_regexp else None
+        if not job_name in logs_by_name:
+            log_path = None
+            ok = False
+        else:
+            log_path = os.path.join(args.logs_folder, logs_by_name[job_name])
+            ok = helpers.check_present(log_path, args.finish_token, args.error_token)
+
+        r = (job_name, job_paths[i], log_path, ok)
+        if values:
+            r += tuple(values)
+
+        if args.check_product:
+            arguments = helpers.argumentize(subfield_names, subfield_positions, values)
+            for product, pattern in args.check_product:
+                target = pattern.format(**arguments)
+                r += (os.path.exists(target),)
+
+        result.append(r)
+
+    columns = ["job_name", "job_path", "log_path", "complete"]
+    if subfield_names:
+        columns += subfield_names
+    if args.check_product:
+        columns_ = [name for name,pattern in args.check_product]
+        columns += columns_
+
+    result = pandas.DataFrame(result, columns=columns)
+    helpers.ensure_requisite_folders(args.output)
+    result.to_csv(args.output, sep="\t", index=False, na_rep="NA")
+
+    if args.resubmit or args.resubmit_log_only or args.clean_target or args.check_product:
+        helpers.post_check(result, args.resubmit, args.resubmit_log_only, args.clean_target, subfield_names, subfield_positions, args.check_product)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("Compare jobs to expected result")
+    parser.add_argument("-jobs_folder", help="Where the submission jobs will be saved", default="jobs")
+    parser.add_argument("-jobs_pattern", help="Job name pattern", default="(.*).sh")
+    parser.add_argument("-logs_folder", help= "Path to results files")
+    parser.add_argument("-logs_pattern", help="Log name pattern", default="(.*)\.e(\d+)\.cri(.*)\.err$")
+    parser.add_argument("-finish_token", help="look for this pattern to deem a run complete")
+    parser.add_argument("--error_token", help="look for this pattern as an error to overrun status")
+    parser.add_argument("-output", help="Path where the output will be saved", default="job_log.txt")
+    parser.add_argument("-parsimony", help="Log verbosity level. 1 is everything being logged. 10 is only high level messages, above 10 will hardly log anything", default = 10, type=int)
+    parser.add_argument("--check_product", help="name of product, and pattern to find it", nargs="+", action="append", default=[])
+    parser.add_argument("--resubmit", help="wether to submit missing jobs", action="store_true")
+    parser.add_argument("--resubmit_log_only", help="wether to submit missing jobs", action="store_true")
+    parser.add_argument("--name_subfield", help="Specify multiple key-value pairs to specify format conversion", nargs=2, action="append", default =[])
+    parser.add_argument("--name_subfield_regexp", help="Specify multiple key-value pairs to specify format conversion")
+    parser.add_argument("--clean_target", help="pattern of stuff to be removed", action="append", default=[])
+
+    #parser.add_argument("--serialize_local", help="If resubmitting job, just run locally", action="store_true", default=False)
+    #parser.add_argument("--log_file", help="On top of everything else, save to a file")
+
+    args = parser.parse_args()
+
+    helpers.configure_logging(int(args.parsimony))
+
+    run(args)
---
+++ src/helpers/helpers.py
@@ -0,0 +1,142 @@
+i__author__ = "alvaro barbeira"
+import logging
+import os
+import sys
+import re
+import glob
+import shutil
+import subprocess
+import time
+
+def configure_logging(level=5, target=sys.stderr, log_file=None):
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # create console handler and set level to info
+    handler = logging.StreamHandler(target)
+    handler.setLevel(level)
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if log_file:
+        fileHandler = logging.FileHandler("log_file")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+
+
+def check_present(path, token, error_token=None):
+    present = False
+    failed = False
+    with open(path) as f:
+        for line in f:
+            if token in line:
+                present = True
+                break
+            if error_token and error_token in line:
+                failed = True
+                break
+    return present and not failed
+
+def clean_target(target):
+    paths = glob.glob(target)
+    if not paths:
+        logging.info("Found nothing applicable to clean")
+    for path in paths:
+        try:
+            if os.path.isdir(path):
+                logging.info("Cleaning folder: %s", path)
+                shutil.rmtree(path)
+            else:
+                logging.info("Cleaning file: %s", path)
+                os.remove(path)
+        except Exception as e:
+            logging.exception("Error with " + path)
+
+def post_check(r, resubmit=False, resubmit_log_only=False, clean_targets=None, subfield_names=None, subfield_positions=None, check_product=None):
+    if r.job_name.shape[0] != r.job_name.drop_duplicates().shape[0]:
+        raise RuntimeError("Won't take duplicate job names")
+
+    redone=[]
+    for t in r.itertuples():
+        if resubmit_log_only and not (type(t.log_path) == str and len(t.log_path) > 1):
+            continue
+
+        redo = False
+        if not t.complete:
+            logging.info("Logs mark this for redo")
+            redo=True
+        elif check_product:
+            for product, pattern in check_product:
+                if not getattr(t,product):
+                    logging.info("Missing product [%s] marks this for redo", product)
+                    redo=True
+                    break
+
+        if redo and resubmit:
+            redone.append(t.job_name)
+            logging.info("Cleaning %s", t.job_name)
+            values = tuple(t)[5:]
+            arguments = argumentize(subfield_names, subfield_positions, values)
+            for target_ in clean_targets:
+                target = target_.format(**arguments)
+                logging.info("Cleaning target: %s", target)
+                clean_target(target)
+
+            for product, pattern in check_product:
+                target = pattern.format(**arguments)
+                logging.info("Cleaning product: %s", target)
+                clean_target(target)
+
+            logging.info("Resubmitting %s", t.job_name)
+            subprocess.call(["qsub", t.job_path])
+            time.sleep(0.1)
+    logging.info("Redid %i", len(redone))
+
+
+def parse_with_regexp(folder, regexp):
+    files = sorted([x for x in os.listdir(folder) if regexp.search(x)])
+    paths = [os.path.join(folder,x) for x in files]
+    names = [regexp.search(x).group(1) for x in files]
+    return names, paths
+
+
+def identify_with_regexp(folder, regexp):
+    files = os.listdir(folder)
+    by_name = {}
+    for f in files:
+        r = regexp.search(f)
+        if r:
+            by_name[r.group(1)] = f
+    return by_name
+
+def ensure_requisite_folders(path):
+    folder = os.path.split(path)[0]
+    if len(folder) and not os.path.exists(folder):
+        os.makedirs(folder)
+
+def name_parse_prepare(name_subfield_regexp, name_subfield):
+    if name_subfield and name_subfield_regexp:
+        r = re.compile(name_subfield_regexp)
+        subfield_names = [x[0] for x in name_subfield]
+        subfield_positions = [int(x[1]) for x in name_subfield]
+    else:
+        r = None
+        subfield_names = None
+        subfield_positions = None
+    return r, subfield_names, subfield_positions
+
+def name_parse(name, subfield_regexp, subfield_positions):
+    if subfield_positions:
+        values = []
+        s_ = subfield_regexp.search(name)
+        for position in subfield_positions:
+            values.append(s_.group(position))
+        values = tuple(values)
+    else:
+        values = None
+    return values
+
+
+def argumentize(subfield_names, subfield_positions, values):
+    return {subfield_names[i-1]:values[i-1] for i in subfield_positions}
